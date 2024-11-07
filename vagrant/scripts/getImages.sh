90f71d1e1cd1de54940e58ebd7e0beb04828e4f6
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-11-07 15:48:06 +0800

    s
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- vagrant/scripts/getImages.sh
+++ vagrant/scripts/getImages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright 2024 Authors of elf-io
+# SPDX-License-Identifier: Apache-2.0
 
 set -o errexit
 set -o nounset
@@ -13,6 +15,8 @@ ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/google_containers
 ORIGIN_IMAGES=$(kubeadm config images list  --kubernetes-version ${K8S_VERSION} 2>/dev/null)
 EXISTED_IMAGES=$(podman images --format "{{.Repository}}:{{.Tag}}" | sed -E 's/[[:space:]]+/:/')
 
+TIMEOUT_SEC=120
+
 echo "---------- origin images: ----------"
 echo  "${ORIGIN_IMAGES} "
 echo ""
@@ -26,7 +30,7 @@ for ITEM in ${IMAGES}; do
 
   echo "---------- pull image: ${ITEM} ----------"
   DOWNLOAD_IMAGE=$(echo "$ITEM" | sed "s?${GCR_URL}?${ALIYUN_URL}?g")
-  podman pull $DOWNLOAD_IMAGE || podman pull $DOWNLOAD_IMAGE || podman pull $DOWNLOAD_IMAGE
+  timeout ${TIMEOUT_SEC} podman pull $DOWNLOAD_IMAGE || timeout ${TIMEOUT_SEC} podman pull $DOWNLOAD_IMAGE || timeout ${TIMEOUT_SEC} podman pull $DOWNLOAD_IMAGE
   podman tag $DOWNLOAD_IMAGE $ITEM
 done
 
@@ -35,7 +39,7 @@ if ! grep -q "${COREDNS_IMAGES}" <<< "${EXISTED_IMAGES}"; then
   echo "---------- pull image: ${COREDNS_IMAGES} ----------"
   # coredns image is special
   DOWNLOAD_DNS_IMAGE=$(echo "${COREDNS_IMAGES}" | sed "s?${GCR_URL}?${ALIYUN_URL}?g" | sed 's?coredns/coredns?coredns?')
-  podman pull $DOWNLOAD_DNS_IMAGE || podman pull $DOWNLOAD_DNS_IMAGE || podman pull $DOWNLOAD_DNS_IMAGE
+  timeout ${TIMEOUT_SEC} podman pull $DOWNLOAD_DNS_IMAGE || timeout ${TIMEOUT_SEC} podman pull $DOWNLOAD_DNS_IMAGE || timeout ${TIMEOUT_SEC} podman pull $DOWNLOAD_DNS_IMAGE
   podman tag $DOWNLOAD_DNS_IMAGE ${COREDNS_IMAGES}
 else
   echo "---------- image existed: ${COREDNS_IMAGES} ----------"
