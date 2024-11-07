d4b1a4503cd09e5c9662407a30f7f190c24a8e8a
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-11-07 16:03:40 +0800

    s
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- vagrant/scripts/installCalico.sh
+++ vagrant/scripts/installCalico.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright 2024 Authors of elf-io
+# SPDX-License-Identifier: Apache-2.0
 
 set -o errexit
 set -o nounset
@@ -16,12 +18,14 @@ VERSION=v3.28.2
 sed -i 's?docker.io?docker.m.daocloud.io?'  ${CURRENT_DIR_PATH}/calico-${VERSION}.yaml
 kubectl apply -f ${CURRENT_DIR_PATH}/calico-${VERSION}.yaml
 
+TIMEOUT_SEC=180
+
 PullImage(){
     IMAGE_LIST=$( cat ${CURRENT_DIR_PATH}/calico-${VERSION}.yaml | grep "image:" | sort | uniq | awk '{print $2}' | tr '\n' ' ' )
     cat <<EOF > ${CURRENT_DIR_PATH}/pull-calico-image.sh
     for  IMAGE in ${IMAGE_LIST}; do
         echo "pull image: \${IMAGE}"
-        podman pull \${IMAGE}
+        timeout ${TIMEOUT_SEC} podman pull \${IMAGE} || timeout ${TIMEOUT_SEC} podman pull \${IMAGE} || timeout ${TIMEOUT_SEC} podman pull \${IMAGE}
     done
 EOF
     chmod +x ${CURRENT_DIR_PATH}/pull-calico-image.sh
